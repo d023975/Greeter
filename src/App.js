e698dfccfb34922b5b4eaa372b54d3173ff614ad
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-05 15:23:16 +0200

    no destructuring in function signature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   const greeting = 'Hello Function Component!';
   return <Headline value={greeting} />;
 }
-function Headline({value}) { //destructuring of pros
-  return <h1>{value}</h1>;
+function Headline(props) { //no destructuring in function signature
+  return <h1>{props.value}</h1>;
 }
 export default App;
