fdf9c8e69091510ceb01605f5dbd2912a94c1d6b
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-05 13:13:34 +0200

    render component inside a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 function App() {
-  const greeting = 'Hello Function Component!';
-  return <h1>{greeting}</h1>;  //return JSX , JavaScript in curly braces
+	return <Headline />; // return component
+}
+function Headline() {
+	const greeting = 'Hello Function Component!';
+	return <h1>{greeting}</h1>;
 }
 export default App;
