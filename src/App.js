37728dfaed0c3c71e20c57c1932990e32fb07052
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-05 13:09:27 +0200

    simple component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import React from "react";
-
-export default function Greeter(props) {
-  const [greeting, setGreeting] = React.useState("");
-
-  function handleGreetClick() {
-    alert(`Hello, ${greeting}`);
-  }
-
-  const charsRemaining = props.maxLength - greeting.length;
-  const greetingInvalid = greeting.length === 0 || charsRemaining < 0;
-
-  return (
-    <div>
-      Greeting:
-      <input
-        className={!greetingInvalid ? `valid` : `invalid`}
-        value={greeting}
-        onChange={e => {
-          setGreeting(e.target.value);
-          console.log(e.target.value);
-          console.log(!greetingInvalid ? `valid` : `invalid`);
-
-        }}
-      />
-      <span style={{ color: !greetingInvalid ? `green` : `red` }}>
-        {charsRemaining}
-      </span>
-      <button disabled={greetingInvalid} onClick={handleGreetClick}>
-        Greet
-      </button>
-    </div>
-  );
+import React from 'react';
+function App() {
+  const greeting = 'Hello Function Component!';
+  return <h1>{greeting}</h1>;  //return JSX , JavaScript in curly braces
 }
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './styles.css';
 
-import Greeter from "./App";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   rootElement
-// );
-ReactDOM.render(<Greeter maxLength={20} />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
+ 
