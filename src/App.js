97a7db906d2a758f0441f7d2f4aa16a21bb4d432
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-06 14:54:41 +0200

    spread syntax 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 function App() {
 	const greeting = {
-    subject: 'React',
-    description: 'Your component library for ...',
-  };
+		subject: 'React',
+		description: 'Your component library for ...'
+	};
 	return (
 		<div>
-			<Headline value={{...greeting}} />
+			<Headline {...greeting} />
 		</div>
 	);
 }
-function Headline({ value : val }) {
-
-  const Title = ({ title }) => <h1>{title}</h1>;
-  const Description = ({ description }) => <p>{description}</p>;
+function Headline({ subject, description }) {
+	const Title = ({ title }) => <h1>{title}</h1>;
+	const Description = ({ description }) => <p>{description}</p>;
 
 	return (
-    <div>
-      <Title title={`Welcome to ${val.subject}`} />
-      <Description description={val.description} />
-    </div>
-  
-  );
+		<div>
+			<Title title={`Welcome to ${subject}`} />
+			<Description description={description} />
+		</div>
+	);
 }
 export default App;
