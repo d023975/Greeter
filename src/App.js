50525a7aba8e8679ca4d3642e6f167c31974f7b8
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-07 15:24:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from "react";
+import React from 'react';
 
 export default function Greeter(props) {
-  const [greeting, setGreeting] = React.useState("");
+	const [ greeting, setGreeting ] = React.useState('');
 
-  function handleGreetClick() {
-    alert(`Hello, ${greeting}`);
-  }
+	function handleGreetClick() {
+		alert(`Hello, ${greeting}`);
+	}
 
-  const charsRemaining = props.maxLength - greeting.length;
-  const greetingInvalid = greeting.length === 0 || charsRemaining < 0;
+	const charsRemaining = props.maxLength - greeting.length;
+	const greetingInvalid = greeting.length === 0 || charsRemaining < 0;
 
-  return (
-    <div>
-      Greeting:
-      <input
-        className={!greetingInvalid ? `valid` : `invalid`}
-        value={greeting}
-        onChange={e => {
-          setGreeting(e.target.value);
-          console.log(e.target.value);
-          console.log(!greetingInvalid ? `valid` : `invalid`);
+	return (
+		<div>
+			Greeting:
+			<input
+				className={!greetingInvalid ? `valid` : `invalid`}
+				value={greeting}
+				onChange={(e) => {
+					setGreeting(e.target.value);
+					console.log(e.target.value);
+					console.log(!greetingInvalid ? `valid` : `invalid`);
+				}}
+			/>
+			<span style={{ color: !greetingInvalid ? `green` : `red` }}>{charsRemaining}</span>
+			<button disabled={greetingInvalid} onClick={handleGreetClick}>
+				Greet
+			</button>
+		</div>
+	);
+}
 
-        }}
-      />
-      <span style={{ color: !greetingInvalid ? `green` : `red` }}>
-        {charsRemaining}
-      </span>
-      <button disabled={greetingInvalid} onClick={handleGreetClick}>
-        Greet
-      </button>
-    </div>
-  );
+export function Width({ width }) {
+	return <div>window is {width}</div>;
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import './styles.css';
 
 import Greeter from "./App";
+import {Width} from "./App";
 
 const rootElement = document.getElementById("root");
+const yar = document.getElementById("yar");
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
@@ -12,3 +14,4 @@ const rootElement = document.getElementById("root");
 //   rootElement
 // );
 ReactDOM.render(<Greeter maxLength={20} />, rootElement);
+ReactDOM.render(<Width width={20} />, yar);
